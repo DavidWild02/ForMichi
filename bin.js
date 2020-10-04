28a6e8c9b4c00a12fdf42d0106422a9418f1c8ab
David Wild <david.wild2002@gmail.com>
2020-10-04 14:12:26 +0200

    now is 999 standard

--- bin.js
+++ bin.js
@@ -3,6 +3,6 @@ const extractData = require('.');
 const args = process.argv.slice(2);
 const FILE = args[0] || 'trains.json';
 const MIN = parseInt(args[1]) || 0;
-const MAX = parseInt(args[2]) || 200;
+const MAX = parseInt(args[2]) || 999;
 
 extractData(FILE, MIN, MAX);
